e0040d345ef39317a80d9603a50b1ab03025eaa3
jeremief <quasimobeau@yahoo.fr>
2018-01-03 08:33:07 +1100

    Feat: make enemies render Need to put the render function back into the prototype

--- js/app.js
+++ js/app.js
@@ -1,35 +1,50 @@
 // Enemies our player must avoid
-var Enemy = function() {
+// var Enemy = function() {
+var Enemy = function(xCoordinates, yCoordinates, name) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    return this;
+    // this.sprite = 'images/enemy-bug.png';
+    var obj = {
+        x: xCoordinates,
+        y: yCoordinates,
+        sprite: 'images/enemy-bug.png',
+        render: function() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        // ctx.drawImage(Resources.get(this.sprite), 3, 300);
+        // console.log(`${name} rendered`);
+        },
+        update: function(){}
+    }
+    return obj;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+// Enemy.prototype.update = function(dt) {
+//     // You should multiply any movement by the dt parameter
+//     // which will ensure the game runs at the same speed for
+//     // all computers.
+// };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+// Enemy.prototype.render = function() {
+//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+//     console.log('enemy rendered');
+// };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
 var Player = function(){
+
     this.sprite = 'images/char-boy.png';
     this.update = function(){};
-    this.render = function(){};
+    this.render = function(){
+    };
     this.handleInput = function(){};
 
 
@@ -40,7 +55,13 @@ var Player = function(){
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
+var enemy1 = Enemy(1,1, 'enemy1');
+var enemy2 = Enemy(50, 60, 'enemy2');
+var allEnemies = [enemy1, enemy2];
+// allEnemies.push(enemy1);
+// console.log(enemy1);
+// console.log(allEnemies);
+
 var player = Player();
 
 
